0451aba4cdda5a602878ebb6b0c951e4d949873a
Ian Moore <ian.moore@rate.com>
2019-02-15 10:49:59 -0600

    use grandshell theme

--- init.el
+++ init.el
@@ -20,14 +20,15 @@
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-
+;; preferred emacs theme
+(load-theme 'grandshell t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit neotree))))
+ '(package-selected-packages (quote (grandshell-theme magit neotree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
