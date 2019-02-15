fe8da4d0e51809856868bc785fc9e38b98c3e81a
Ian Moore <ian.moore@rate.com>
2019-02-15 13:18:36 -0600

    trying to get neotree refresh to work

--- init.el
+++ init.el
@@ -21,6 +21,10 @@
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;; Projectile
+(require 'projectile)
+(projectile-mode +1)
+
 ;; preferred emacs theme
 (load-theme 'grandshell t)
 
@@ -29,7 +33,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (grandshell-theme magit neotree))))
+ '(package-selected-packages (quote (projectile grandshell-theme magit neotree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- neotree.el
+++ neotree.el
@@ -1,3 +1,4 @@
 (require 'neotree)
 (global-set-key (kbd "<f8> <f8>") 'neotree-toggle)
+(setq neo-autorefresh t)
 (setq neo-smart-open t)
